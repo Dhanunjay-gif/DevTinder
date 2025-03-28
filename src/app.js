8cbbb6d58a4026c13302f35a016729ef61eb6ab0
dhanu <boyadhanunjay01@gmail.com>
2025-03-28 10:36:18 +0530

    new changes

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app =express()
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'], 
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
